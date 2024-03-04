cd502567e252604c35e49be17401703e6f61b957
idodohan <ido@dohan.co.il>
2024-03-04 19:21:34 +0200

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,8 +1,7 @@
 import hashlib
 
-from django import forms
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
