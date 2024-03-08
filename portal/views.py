90ba27a1bc6b6edd82389a425cb2988ee11f67ef
idodohan <ido@dohan.co.il>
2024-03-08 07:45:19 +0200

    removed comment

--- portal/views.py
+++ portal/views.py
@@ -10,23 +10,6 @@ def dashboard(request):
 
 def add_customer(request):
     if request.method == 'POST':
-        '''
-        NOT HERE THO. sohuld be on tasks 1, 3, 4
-        ___Customer.objects.raw(request.text())___ -> this is not exactly correct. it needs to build  a sql query from requst.POST. youll figure it out
-
-        request.text() can be good; "ADD Customer email:viktor@blal, name:vik" -> adds a new customer
-                but can also be bad; "DELETE *"                                -> deletes a customer
-        
-        this occurs because the query is passed as 'raw' and we can send a query as an argument of the request
-        
-        the way to fix the vulnerability is to 'parametize' the query in this manner:
-        
-        ___CustomerForm(request.POST)___
-        Explanation:
-        the request now MUST recieve the following parameters: (email: str, name: str)
-        thus we are unable to pass a query 'as is'
-        '''
-
         form = CustomerForm(request.POST)
         if form.is_valid():
             form.save()
